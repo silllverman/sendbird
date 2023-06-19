ee6ea88ba345d22c65dbb56cb9d0d3190d5c1c9a
silllverman <aas@popoo.io>
2023-06-19 17:36:24 +0330

    fix: :bug: fix input box issues

--- uikit-react-native/src/components/ChannelInput/SendInput.tsx
+++ uikit-react-native/src/components/ChannelInput/SendInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef } from "react";
 import {
   NativeSyntheticEvent,
   Platform,
@@ -6,9 +6,9 @@ import {
   TextInputSelectionChangeEventData,
   TouchableOpacity,
   View,
-} from 'react-native';
+} from "react-native";
 
-import { MentionType } from '@sendbird/chat/message';
+import { MentionType } from "@sendbird/chat/message";
 import {
   Icon,
   TextInput,
@@ -17,20 +17,26 @@ import {
   useBottomSheet,
   useToast,
   useUIKitTheme,
-} from '@sendbird/uikit-react-native-foundation';
-import { isImage, shouldCompressImage } from '@sendbird/uikit-utils';
+} from "@sendbird/uikit-react-native-foundation";
+import { isImage, shouldCompressImage } from "@sendbird/uikit-utils";
 
-import { useLocalization, usePlatformService, useSendbirdChat } from '../../hooks/useContext';
-import SBUError from '../../libs/SBUError';
-import SBUUtils from '../../libs/SBUUtils';
-import type { FileType } from '../../platform/types';
-import type { MentionedUser } from '../../types';
-import type { ChannelInputProps } from './index';
+import {
+  useLocalization,
+  usePlatformService,
+  useSendbirdChat,
+} from "../../hooks/useContext";
+import SBUError from "../../libs/SBUError";
+import SBUUtils from "../../libs/SBUUtils";
+import type { FileType } from "../../platform/types";
+import type { MentionedUser } from "../../types";
+import type { ChannelInputProps } from "./index";
 
 interface SendInputProps extends ChannelInputProps {
   text: string;
   onChangeText: (val: string) => void;
-  onSelectionChange: (e: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => void;
+  onSelectionChange: (
+    e: NativeSyntheticEvent<TextInputSelectionChangeEventData>
+  ) => void;
   mentionedUsers: MentionedUser[];
 }
 
@@ -48,9 +54,10 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
     inputFrozen,
     inputMuted,
   },
-  ref,
+  ref
 ) {
-  const { mentionManager, imageCompressionConfig, features } = useSendbirdChat();
+  const { mentionManager, imageCompressionConfig, features } =
+    useSendbirdChat();
   const { STRINGS } = useLocalization();
   const { fileService, mediaService } = usePlatformService();
   const { colors } = useUIKitTheme();
@@ -61,7 +68,8 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
   const sendUserMessage = () => {
     const mentionType = MentionType.USERS;
     const mentionedUserIds = mentionedUsers.map((it) => it.user.userId);
-    const mentionedMessageTemplate = mentionManager.textToMentionedMessageTemplate(text, mentionedUsers);
+    const mentionedMessageTemplate =
+      mentionManager.textToMentionedMessageTemplate(text, mentionedUsers);
 
     if (onPressSendUserMessage) {
       onPressSendUserMessage({
@@ -78,7 +86,7 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
       }).catch(onFailureToSend);
     }
 
-    onChangeText('');
+    onChangeText("");
   };
 
   const sendFileMessage = (file: FileType) => {
@@ -89,29 +97,35 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
     }
   };
 
-  const onFailureToSend = () => toast.show(STRINGS.TOAST.SEND_MSG_ERROR, 'error');
+  const onFailureToSend = () =>
+    toast.show(STRINGS.TOAST.SEND_MSG_ERROR, "error");
 
   const onPressAttachment = () => {
     openSheet({
       sheetItems: [
         {
           title: STRINGS.LABELS.CHANNEL_INPUT_ATTACHMENT_CAMERA,
-          icon: 'camera',
+          icon: "camera",
           onPress: async () => {
             const mediaFile = await fileService.openCamera({
-              mediaType: 'all',
+              mediaType: "all",
               onOpenFailure: (error) => {
                 if (error.code === SBUError.CODE.ERR_PERMISSIONS_DENIED) {
                   alert({
                     title: STRINGS.DIALOG.ALERT_PERMISSIONS_TITLE,
                     message: STRINGS.DIALOG.ALERT_PERMISSIONS_MESSAGE(
                       STRINGS.LABELS.PERMISSION_CAMERA,
-                      STRINGS.LABELS.PERMISSION_APP_NAME,
+                      STRINGS.LABELS.PERMISSION_APP_NAME
                     ),
-                    buttons: [{ text: STRINGS.DIALOG.ALERT_PERMISSIONS_OK, onPress: () => SBUUtils.openSettings() }],
+                    buttons: [
+                      {
+                        text: STRINGS.DIALOG.ALERT_PERMISSIONS_OK,
+                        onPress: () => SBUUtils.openSettings(),
+                      },
+                    ],
                   });
                 } else {
-                  toast.show(STRINGS.TOAST.OPEN_CAMERA_ERROR, 'error');
+                  toast.show(STRINGS.TOAST.OPEN_CAMERA_ERROR, "error");
                 }
               },
             });
@@ -120,7 +134,10 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
               // Image compression
               if (
                 isImage(mediaFile.uri, mediaFile.type) &&
-                shouldCompressImage(mediaFile.type, features.imageCompressionEnabled)
+                shouldCompressImage(
+                  mediaFile.type,
+                  features.imageCompressionEnabled
+                )
               ) {
                 await SBUUtils.safeRun(async () => {
                   const compressed = await mediaService.compressImage({
@@ -143,23 +160,28 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
         },
         {
           title: STRINGS.LABELS.CHANNEL_INPUT_ATTACHMENT_PHOTO_LIBRARY,
-          icon: 'photo',
+          icon: "photo",
           onPress: async () => {
             const mediaFiles = await fileService.openMediaLibrary({
               selectionLimit: 1,
-              mediaType: 'all',
+              mediaType: "all",
               onOpenFailure: (error) => {
                 if (error.code === SBUError.CODE.ERR_PERMISSIONS_DENIED) {
                   alert({
                     title: STRINGS.DIALOG.ALERT_PERMISSIONS_TITLE,
                     message: STRINGS.DIALOG.ALERT_PERMISSIONS_MESSAGE(
                       STRINGS.LABELS.PERMISSION_DEVICE_STORAGE,
-                      STRINGS.LABELS.PERMISSION_APP_NAME,
+                      STRINGS.LABELS.PERMISSION_APP_NAME
                     ),
-                    buttons: [{ text: STRINGS.DIALOG.ALERT_PERMISSIONS_OK, onPress: () => SBUUtils.openSettings() }],
+                    buttons: [
+                      {
+                        text: STRINGS.DIALOG.ALERT_PERMISSIONS_OK,
+                        onPress: () => SBUUtils.openSettings(),
+                      },
+                    ],
                   });
                 } else {
-                  toast.show(STRINGS.TOAST.OPEN_PHOTO_LIBRARY_ERROR, 'error');
+                  toast.show(STRINGS.TOAST.OPEN_PHOTO_LIBRARY_ERROR, "error");
                 }
               },
             });
@@ -170,7 +192,10 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
               // Image compression
               if (
                 isImage(mediaFile.uri, mediaFile.type) &&
-                shouldCompressImage(mediaFile.type, features.imageCompressionEnabled)
+                shouldCompressImage(
+                  mediaFile.type,
+                  features.imageCompressionEnabled
+                )
               ) {
                 await SBUUtils.safeRun(async () => {
                   const compressed = await mediaService.compressImage({
@@ -193,17 +218,21 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
         },
         {
           title: STRINGS.LABELS.CHANNEL_INPUT_ATTACHMENT_FILES,
-          icon: 'document',
+          icon: "document",
           onPress: async () => {
             const documentFile = await fileService.openDocument({
-              onOpenFailure: () => toast.show(STRINGS.TOAST.OPEN_FILES_ERROR, 'error'),
+              onOpenFailure: () =>
+                toast.show(STRINGS.TOAST.OPEN_FILES_ERROR, "error"),
             });
 
             if (documentFile) {
               // Image compression
               if (
                 isImage(documentFile.uri, documentFile.type) &&
-                shouldCompressImage(documentFile.type, features.imageCompressionEnabled)
+                shouldCompressImage(
+                  documentFile.type,
+                  features.imageCompressionEnabled
+                )
               ) {
                 await SBUUtils.safeRun(async () => {
                   const compressed = await mediaService.compressImage({
@@ -240,8 +269,12 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
     <View style={styles.sendInputContainer}>
       <TouchableOpacity onPress={onPressAttachment} disabled={inputDisabled}>
         <Icon
-          color={inputDisabled ? colors.ui.input.default.disabled.highlight : colors.ui.input.default.active.highlight}
-          icon={'add'}
+          color={
+            inputDisabled
+              ? colors.ui.input.default.disabled.highlight
+              : colors.ui.input.default.active.highlight
+          }
+          icon={"add"}
           size={24}
           containerStyle={styles.iconAttach}
         />
@@ -263,9 +296,11 @@ const SendInput = forwardRef<RNTextInput, SendInputProps>(function SendInput(
         <TouchableOpacity onPress={sendUserMessage} disabled={inputDisabled}>
           <Icon
             color={
-              inputDisabled ? colors.ui.input.default.disabled.highlight : colors.ui.input.default.active.highlight
+              inputDisabled
+                ? colors.ui.input.default.disabled.highlight
+                : colors.ui.input.default.active.highlight
             }
-            icon={'send'}
+            icon={"send"}
             size={24}
             containerStyle={styles.iconSend}
           />
@@ -279,14 +314,14 @@ const styles = createStyleSheet({
   sendInputContainer: {
     paddingVertical: 10,
     paddingHorizontal: 12,
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
   },
   input: {
     flex: 1,
     marginRight: 4,
-    minHeight: 36,
-    maxHeight: 36 * Platform.select({ ios: 2.5, default: 2 }),
+    // minHeight: 36,
+    // maxHeight: 36 * Platform.select({ ios: 2.5, default: 2 }),
     borderRadius: 20,
   },
   iconAttach: {
